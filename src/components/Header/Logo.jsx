886d9775d0c4b9eaefdc56b009100684ec35c2cc
Nicolas Moron <nmoron6@gmail.com>
2024-12-20 17:15:50 -0400

    change-logo

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,12 +1,12 @@
 import { logo } from "../../images";
 function Logo() {
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center space-x-2 max-h-20">
       <a href="#" className="flex items-center space-x-2">
         <img
           src={logo}
           alt="logo"
-          className="w-[4em] h-[4em] bg-white rounded-full m-1"
+          className=" bg-white rounded-full m-1 h-auto max-w-20"
         />
         <span className="text-2xl font-bold text-gray-800 dark:text-white font-Pixelify tracking-widest">
           NICO.ME
